3f67f0134db8ea37a6baa466de77cbfe147fc1f6
ayush singh <singhayush886@gmail.com>
2023-02-19 21:43:54 +0530

    learning

--- JAVASCRIPT/Baiscs/script.js
+++ JAVASCRIPT/Baiscs/script.js
@@ -1,3 +1,12 @@
 var myName="Ayush Singh";
 var myAge=21;
 console.log(myName+" "+myAge);
+
+// Variables
+// variables is a container to store the values 
+// data types
+// var ,let,const 
+let num1=45;
+let num2=7;
+let ans=num1*num2;
+console.log(ans);
