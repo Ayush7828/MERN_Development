7f1192979644628e68d9894b64e09fae5a1ec26f
ayush singh <singhayush886@gmail.com>
2023-02-23 22:31:31 +0530

    learning_webdevelopment

--- JAVASCRIPT/Baiscs/script.js
+++ JAVASCRIPT/Baiscs/script.js
@@ -1,12 +1,19 @@
-var myName="Ayush Singh";
-var myAge=21;
-console.log(myName+" "+myAge);
+// var myName="Ayush Singh";
+// var myAge=21;
+// console.log(myName+" "+myAge);
 
 // Variables
 // variables is a container to store the values 
 // data types
 // var ,let,const 
-let num1=45;
-let num2=7;
-let ans=num1*num2;
-console.log(ans);
+// let is use for modern web  development 
+// let num1=45;
+// let num2=7;
+// let ans=num1*num2;
+// console.log(ans);
+
+// const a=45; maximum time use in website  
+// console.log(a);
+
+
+
